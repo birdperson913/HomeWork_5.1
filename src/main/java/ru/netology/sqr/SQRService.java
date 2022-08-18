b65fc53d83ed86dc826aa099f46cd44442a7ebe9
birdperson913 <81969845+birdperson913@users.noreply.github.com>
2022-08-18 19:07:50 +0300

    Add new

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -3,7 +3,7 @@ package ru.netology.sqr;
 public class SQRService {
     public int summSquareRoot(int lowerBound, int upperBound) {
         int numberOfSquares = 0;
-        for (int i = 0; i < upperBound; i++) {
+        for (int i = 10; i <100; i++) {
             if (i * i <= upperBound && i * i >= lowerBound) {
                 numberOfSquares++;
             }
